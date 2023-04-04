41b1fdfbf5f0a280c1cc4cbb5612f49eecd0181a
Arun Suresh <arunsuresh241@gmail.com>
2023-04-04 22:39:15 +0530

    save changes made in exercise page

--- strong/exercises/urls.py
+++ strong/exercises/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from .import views
 urlpatterns = [
     path('about-exercise/<str:id>',views.aboutExercise,name="about-exercise"),
+    path('exercise-history/<str:id>',views.exercise_history,name='exercise-history'),
+
 ]
--- strong/exercises/views.py
+++ strong/exercises/views.py
@@ -1,11 +1,46 @@
 from django.shortcuts import render
-from home.models import Exercise
+from django.http import JsonResponse,Http404
+
+from home.models import Exercise,Set,WorkoutSessionExercise,WorkoutSession
+from django.db.models import Prefetch
+from workout.views import get_response_dict
+from django.core import serializers
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 
 # Create your views here.
 
 def aboutExercise(request,id):
     exercise = Exercise.objects.get(pk=id)
+    # Get the logged-in user
     context = {
-        'exercise':exercise
+        'exercise':exercise,
     }
     return render(request,'exercises/aboutExercise.html',context)
+
+
+
+def exercise_history(request, id):
+    try:
+        user = request.user
+
+        # Use a single query to retrieve the necessary data
+
+        if not WorkoutSession.objects.filter(user=user, workoutsessionexercise__exercise__id=id).exists():
+            return JsonResponse(get_response_dict('warning','Not performed yet'))
+
+        exercise_session = WorkoutSession.objects.filter(user=user, workoutsessionexercise__exercise__id=id).values('name', 'started_at', 'workoutsessionexercise__id')
+        exercise_session = list(exercise_session)
+
+        # add sets to each exercise session
+        for i in exercise_session:
+            sets = Set.objects.filter(exercise_session=i['workoutsessionexercise__id']).values('set_number', 'weight', 'reps')
+            i['sets'] = list(sets)
+
+        return JsonResponse(get_response_dict('success','success',exercise_session))  # return the serialized data in a JsonResponse
+
+    except Exception as e:
+        # Handle the error appropriately, e.g. log it, return an error response, etc.
+        print(f"Error occurred: {e}")
+        raise Http404("Exercise history not found")
+
